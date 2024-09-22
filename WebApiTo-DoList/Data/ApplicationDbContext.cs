8ba3fe2a0a936363911a389405f0606c8c74c6e4
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-09-22 18:55:11 -0300

    Feat: Aplicando configurações iniciais, adicionando pakages, criando entidades e configurando banco de dados.

---
+++ WebApiTo-DoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiTo_DoList.Models;
+
+namespace WebApiTo_DoList.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<UsuarioModel> Usuario { get; set; }
+    public DbSet<TarefaModel> Tarefa { get; set; }
+}
