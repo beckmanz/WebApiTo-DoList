c6b7737c37f23bb020cf310d607a444c33b697a4
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-09-30 15:47:22 -0300

    Feat: Adicionei um IgnoreCycles para evitar ciclos infinitos ao usar alguns métodos.

--- WebApiTo-DoList/Program.cs
+++ WebApiTo-DoList/Program.cs
@@ -1,17 +1,24 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using WebApiTo_DoList.Data;
+using WebApiTo_DoList.Models;
+using WebApiTo_DoList.Services.Tarefa;
 using WebApiTo_DoList.Services.Usuario;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUsuarioInterface, UsuarioServices>();
+builder.Services.AddScoped<ITarefaInterface, TarefaServices>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
